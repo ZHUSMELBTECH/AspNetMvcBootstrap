48539a29f9c1a9ba30e092ba77671bd5b71afafe
Yu Zhu <devop228@zhusmelb.com>
2019-10-05 20:53:30 +1000

    publish nuget package

--- .build/build.ps1
+++ .build/build.ps1
@@ -1,3 +1,4 @@
 $gitversionObj = gitversion | ConvertFrom-Json
 $nugetVersion = "$($gitversionObj.MajorMinorPatch)-$($gitversionObj.PreReleaseLabel)"
 nuget pack -OutputDirectory ./.packages -Properties version=$nugetVersion
+Write-Host "##vso[task.setvariable variable=packageFileName]Zhusmelb.AspNet.Mvc.Bootstrap.$(nugetVersion).nupkg"
