2e321e0293de747836b0597ab089b9a2d3c01c9a
Yu Zhu <devop228@zhusmelb.com>
2019-10-07 22:32:14 +1100

    Use nugetcommand task to pack

--- .build/build.ps1
+++ .build/build.ps1
@@ -1,4 +1,5 @@
 $gitversionObj = gitversion | ConvertFrom-Json
 $nugetVersion = "$($gitversionObj.MajorMinorPatch)-$($gitversionObj.PreReleaseLabel)+$($gitversionObj.BuildMetaData)"
-nuget pack .\PackageContent\Package.nuspec -BasePath .\PackageContent\ -OutputDirectory ./.packages -Version $nugetVersion
-Write-Host "##vso[task.setvariable variable=packageFileName]Zhusmelb.AspNet.Mvc.Bootstrap.$nugetVersion.nupkg"
+$env:packageVersion = $nugetVersion
+#nuget pack .\PackageContent\Package.nuspec -BasePath .\PackageContent\ -OutputDirectory ./.packages -Version $nugetVersion
+Write-Host "##vso[task.setvariable variable=packageVersion]$nugetVersion"
