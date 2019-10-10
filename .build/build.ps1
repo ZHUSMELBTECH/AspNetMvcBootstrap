71826aab21c7761dcd9eb26759cf340283ef4f79
Yu Zhu <devop228@zhusmelb.com>
2019-10-10 21:36:41 +1100

    Update feature branches to ContinuousDeployment, update SemVer every build.

--- .build/build.ps1
+++ .build/build.ps1
@@ -1,5 +1,7 @@
+Push-Location -Path .\.build
 $gitversionObj = gitversion | ConvertFrom-Json
-$nugetVersion = "$($gitversionObj.MajorMinorPatch)-$($gitversionObj.PreReleaseLabel)+$($gitversionObj.BuildMetaData)"
+Pop-Location
+$nugetVersion = "$($gitversionObj.SemVer)"
 $env:packageVersion = $nugetVersion
 #nuget pack .\PackageContent\Package.nuspec -BasePath .\PackageContent\ -OutputDirectory ./.packages -Version $nugetVersion
 Write-Host "##vso[task.setvariable variable=packageVersion]$nugetVersion"
